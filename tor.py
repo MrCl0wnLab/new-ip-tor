95d7e2ad6ea216737bc41705af9fdee924771758
Mr. Cl0wn - H4ck1ng C0d3r <0x27null@gmail.com>
2020-04-06 23:50:45 -0300

    Update tor.py

--- tor.py
+++ tor.py
@@ -4,26 +4,38 @@ import requests
 import threading
 import time
 
-IP_HOST_TOR = '127.0.0.1'
-IP_PORT_TOR = '9050'
-HOST_TEST_TOR = 'http://ifconfig.me'
-DEBUG = False
 
+IP_HOST_TOR = '127.0.0.1'
+# tor control port as defined in torrc file
+IP_PORT_TOR = '9050'
+# host test request "new identity" IP
+HOST_TEST_TOR = 'http://ifconfig.me'
+# flag debug "new identity" IP
+DEBUG = False
+# config socks5 ip + port default tor
 CONFIG_PROXY_TOR = {
     'http':  f'socks5://{IP_HOST_TOR}:{IP_PORT_TOR}',
     'https': f'socks5://{IP_HOST_TOR}:{IP_PORT_TOR}'
 }
 
-
+# view result "new identity" IP
 def view_new_ip(url):
-    datetime_now = datetime.datetime.now()
-    html = requests.get(url, proxies=CONFIG_PROXY_TOR)
-    print(f"[{datetime_now}] [ RESULT REQUEST ] {html.text}")
+    try:
+        datetime_now = datetime.datetime.now()
+        html = requests.get(url, proxies=CONFIG_PROXY_TOR)
+        print(f"[{datetime_now}] [ RESULT REQUEST ] {html.text}")
+    except:
+        return False
 
 
+# reboot execution of the TOR service.
+# tor daemon re-read configurations files and make "new identity".
 def kill_tor():
-    # REF https://github.com/googleinurl/SCANNER-INURLBR/blob/master/inurlbr.php#L1450
-    os.system("[ -z 'pidof tor' ] || pidof tor | xargs sudo kill -HUP;")
+    try:
+        # REF https://github.com/googleinurl/SCANNER-INURLBR/blob/master/inurlbr.php#L1450
+        os.system("[ -z 'pidof tor' ] || pidof tor | xargs sudo kill -HUP;")
+    except:
+        return False
 
 
 def new_ip_tor():
@@ -32,4 +44,3 @@ def new_ip_tor():
     threading.Thread(target=kill_tor).start()
     if DEBUG:
         threading.Thread(target=view_new_ip, args=(HOST_TEST_TOR,)).start()
-
